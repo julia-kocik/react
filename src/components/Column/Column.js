5b841d89be157c579a9104362043304d557cb60d
Julia <julia.kocik02@gmail.com>
2021-04-29 17:23:28 +0200

    Add use value from state

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 class Column extends React.Component {
     static propTypes = {
-        columnText: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
     }
     render() {
         return (
             <section className={styles.component}>
                 <h3 className={styles.title}>
-                    {this.props.columnText}
+                    {this.props.title}
                 </h3>
             </section>
         )
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,27 +3,33 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import Column from '../Column/Column';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 
 class List extends React.Component {
+    state = {
+        columns: this.props.columns || [],
+    }
     static propTypes = {
         title: PropTypes.node.isRequired,
-        children: PropTypes.node,
+        description: PropTypes.node,
+        columns: PropTypes.array,   
     }
     static defaultProps = {
-      children: <p>I can do all the things!!!</p>,
+        description: settings.defaultListDescription,
     }
     render() {
         return (
         <section className={styles.component}>
-            <Hero titleText={this.props.title} imageSrc={this.props.imgSrc}/>
+            <Hero titleText={this.props.title} imageSrc={this.props.image}/>
             <div className={styles.description}>
-                {this.props.children}
+                {ReactHtmlParser(this.props.description)}
             </div>
             <div className={styles.columns}>
-                <Column columnText="Animals"/>
-                <Column columnText="Plants"/>
-                <Column columnText="Minerals"/>
+                {this.state.columns.map(({key, ...columnProps}) => (
+                    <Column key={key} {...columnProps} />
+                ))}
             </div>
         </section>
         )
