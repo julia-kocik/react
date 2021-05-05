8ca333ba006690c8ac2e69505b5cb7bb05cfff69
Włodek Kocik <julia.kocik02@gmail.com>
2021-05-05 16:25:23 +0200

    Clean code

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -46,15 +46,9 @@ class Column extends React.Component {
           ))}
         </div>
         <div className={styles.creator}>
-
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-
         </div>
-
-
-
       </section>
-
     )
   }
 }
